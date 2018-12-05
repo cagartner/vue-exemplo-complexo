9fa4097be88fbdfbdba41a371a44407406c61b73
Carlos Augusto Gartner <contato@carlosgartner.com.br>
2018-12-05 17:46:13 -0200

    Develop example

---
+++ web/app/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="helloworld">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HelloWorld",
+        data() {
+            return {
+                message: 'Bem Vindo!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .helloworld {
+        width: 100%;
+        float: left;
+        background: #ccc;
+        font-size: 2rem;
+        margin-top: 60px;
+        text-align: center;
+    }
+</style>
---
+++ web/app/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+import HelloWorld from './components/HelloWorld'
+
+new Vue({ // eslint-disable-line no-new
+    el: '#app',
+    components: {
+        HelloWorld
+    }
+})
